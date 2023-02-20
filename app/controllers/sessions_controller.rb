8146c6b9f749b8ef76807bbe4761dc0358f1abfa
Gary <geehor@gmail.com>
2023-02-20 15:36:39 -0500

    Continue Sessions Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,9 @@ class SessionsController < ApplicationController
         render :new
     end
 
-    
+    def create
+        user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        session[:session_token] = user.reset_session_token!
+        redirect_to cats_url
+    end
 end
